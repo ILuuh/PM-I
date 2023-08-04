8c81dd31a0af0de7ccafab50ccffe771cd4bff67
unknown <luiz.gouvea2b2@gmail.com>
2023-08-04 20:52:09 -0300

    branch com o código da aula do dia Fri, Aug  4, 2023  8:52:08 PM

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage ({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children:  [
+            const Text("Hello World!"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Text("Luiz Gustavo"),
+                Text("2023")
+              ],
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class Aplicacao extends StatelessWidget {
+  const Aplicacao({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "App Hello World",
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+void main() => runApp(const Aplicacao());
