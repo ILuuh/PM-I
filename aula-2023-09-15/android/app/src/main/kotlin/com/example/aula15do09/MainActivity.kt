ea06062a674e26db398382892a1d681d464c0264
Luiz Gustavo <125077613+ILuuh@users.noreply.github.com>
2023-09-22 20:50:23 -0300

    Delete aula-2023-09-15 directory

--- aula-2023-09-15/android/app/src/main/kotlin/com/example/aula15do09/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.aula15do09
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- aula-2023-09-15/lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:aula15do09/app.dart';
-import 'package:flutter/material.dart';
-
-void main() => runApp(const Aplicacao());
