6f6cf1f48cbb8c6c728e9d18ff3408e73b7b58d4
unknown <luiz.gouvea2b2@gmail.com>
2023-11-24 20:47:07 -0300

    branch com o código da aula do dia Fri, Nov 24, 2023  8:47:06 PM

---
+++ lib/catalog_view_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:loja/product.dart';
+import 'package:loja/product_repository.dart';
+
+class CatalogViewModel extends ChangeNotifier {
+  late List<Product> products;
+
+  CatalogViewModel(){
+    products = ProductRepository.list();
+  }
+  
+}
---
+++ lib/util.dart
@@ -0,0 +1,8 @@
+import 'package:intl/intl.dart';
+
+final _formatter = NumberFormat.currency(
+  locale: "pt_BR",
+  symbol: "R\$",
+);
+
+String formatCurrency(double value) => _formatter.format(value);
